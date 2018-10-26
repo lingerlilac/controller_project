c43710a545acf794d35f0c488e80dc7b5cbe4d77
lin <lin@lin>
2018-10-26 16:59:36 -0700

    modified

--- tcp_info_get/tcp/client.c
+++ tcp_info_get/tcp/client.c
@@ -33,28 +33,23 @@ static void get_systime_linger(__be32 *t, __be32 *v)
 	*v = tv.tv_usec;
 }
 
-int main(int argc,const char* argv[])
+int ppp(int port_linger, char *ip_linger)
 {
-	if(argc != 3)
-	{
-		printf("Usage:%s [ip] [port]\n",argv[0]);
-		return 0;
-	}
 
 	//创建一个用来通讯的socket
-	int sock = socket(AF_INET,SOCK_STREAM, 0);
+	int sock = socket(AF_INET,SOCK_DGRAM, 0);
 	if(sock < 0)
 	{
 		perror("socket");
-		return 1;
+		exit(-1);
 	}
 
 	//需要connect的是对端的地址，因此这里定义服务器端的地址结构体
 	struct sockaddr_in server;
 	struct sockaddr_in local;
 	server.sin_family = AF_INET;
-	server.sin_port = htons(atoi(argv[2]));
-	server.sin_addr.s_addr = inet_addr(argv[1]);
+	server.sin_port = htons(port_linger);
+	server.sin_addr.s_addr = inet_addr(ip_linger);
 	socklen_t len = sizeof(struct sockaddr_in);
 
 	local.sin_family = AF_INET;
@@ -69,53 +64,78 @@ int main(int argc,const char* argv[])
 		exit(2);
 	}
 
-	if(connect(sock, (struct sockaddr*)&server, len) < 0 )
-	{
-		perror("connect");
-		return 2;
-	}
+	// if(connect(sock, (struct sockaddr*)&server, len) < 0 )
+	// {
+	// 	perror("connect");
+	// 	exit(-1);
+	// }
 	//连接成功进行收数据
+	
+	// check_cwnd(sock);
+	// close(sock);
+
+	return sock;
+}
+
+int check_cwnd(int sock)
+{
 	char buf[1024];
-	while(1)
+	__u32 sec0 = 0;
+	__u32 usec0 = 0;
+	__u32 sec1 = 0, usec1 = 0;
+	__u32 duration = 0;
+	struct tcp_ss *tmp = NULL;
+	char ip_src[] = "192.168.11.107";
+	char ip_dst[] = "180.208.65.23";
+	__be16 src_port = 45036;
+	__be16 dst_port = 80;
+	int len_ss;
+	ssize_t _s = 0;
+	struct sockaddr_in raddr;
+	int val = sizeof(struct sockaddr);
+		
+	len_ss = sizeof(struct tcp_ss) * sizeof(char);
+	tmp = (struct tcp_ss *)malloc(len_ss);
+	memset(tmp, 0, len_ss);
+
+	strcpy(tmp->ip_src, ip_src);
+	strcpy(tmp->ip_dst, ip_dst);
+	tmp->src_port = src_port;
+	tmp->dst_port = dst_port;
+	memcpy(buf, tmp, len_ss);
+	write(sock, buf, len_ss);
+	sleep(1);
+	get_systime_linger(&sec0, &usec0);
+	_s=recvfrom(sock,buf,sizeof(buf)-1,0,(struct sockaddr*)&raddr,&val);
+	// _s = read(sock, buf, sizeof(buf)-1);
+	if(_s > 0)
 	{
-		__u32 sec0 = 0;
-		__u32 usec0 = 0;
-		__u32 sec1 = 0, usec1 = 0;
-		__u32 duration = 0;
-		struct tcp_ss *tmp = NULL;
-		char ip_src[] = "192.168.11.103";
-		char ip_dst[] = "192.168.11.102";
-		__be16 src_port = 12345;
-		__be16 dst_port = 12346;
-		int len_ss;
-		ssize_t _s = 0;
-
-		len_ss = sizeof(struct tcp_ss) * sizeof(char);
-		tmp = (struct tcp_ss *)malloc(len_ss);
-		memset(tmp, 0, len_ss);
-
-		strcpy(tmp->ip_src, ip_src);
-		strcpy(tmp->ip_dst, ip_dst);
-		tmp->src_port = src_port;
-		tmp->dst_port = dst_port;
-		memcpy(buf, tmp, len_ss);
-		write(sock, buf, len_ss);
-		sleep(1);
-		get_systime_linger(&sec0, &usec0);
-		_s = read(sock, buf, sizeof(buf)-1);
-		if(_s > 0)
-		{
-			struct cwnd_mss tmp;
-			int leng_int = sizeof(struct cwnd_mss);
-			memset(&tmp, 0, leng_int);
-			memcpy(&tmp, buf, leng_int);
-			printf("window is %d \t %d\n", tmp.cwnd, tmp.mss);
-		}
-		get_systime_linger(&sec1, &usec1);
-		duration = (sec1 - sec0) * 1000000 + (usec1 - usec0);
-		printf("%u\n", duration);
+		struct cwnd_mss tmp;
+		int leng_int = sizeof(struct cwnd_mss);
+		memset(&tmp, 0, leng_int);
+		memcpy(&tmp, buf, leng_int);
+		printf("window is %d \t %d\n", tmp.cwnd, tmp.mss);
 	}
-	close(sock);
+	get_systime_linger(&sec1, &usec1);
+	duration = (sec1 - sec0) * 1000000 + (usec1 - usec0);
+	printf("%u\n", duration);
 
+}
+int main()
+{
+	char ip[] = "192.168.11.107";
+	int port = 12345;
+	int sock = -1; 
+	if(sock < 0)
+		sock = ppp(port, ip);
+	if(sock > 0)
+	{
+		while(1)
+		{
+			check_cwnd(sock);
+		}
+	}
+	
+	close(sock);
 	return 0;
 }
--- tcp_info_get/tcp/server.c
+++ tcp_info_get/tcp/server.c
@@ -10,6 +10,7 @@
 
 #define _LINE_LENGTH 300
 
+int sock = 0;
 struct tcp_ss
 {
     __u32 state;
@@ -45,9 +46,9 @@ int strlinger ( char *buf, char *sub)
     return -1;
 }
 
-int startup(int _port,const char* _ip)
+int startup(int _port)
 {
-	int sock = socket(AF_INET,SOCK_STREAM,0);
+	sock = socket(AF_INET,SOCK_DGRAM,0);
 	if(sock < 0)
 	{
 		perror("socket");
@@ -60,7 +61,7 @@ int startup(int _port,const char* _ip)
 	struct sockaddr_in local;
 	local.sin_family = AF_INET;
 	local.sin_port = htons( _port);
-	local.sin_addr.s_addr = inet_addr(_ip);
+	local.sin_addr.s_addr = htonl(INADDR_ANY);
 	socklen_t len = sizeof(local);
 
 	if(bind(sock,(struct sockaddr*)&local , len) < 0)
@@ -69,227 +70,202 @@ int startup(int _port,const char* _ip)
 		exit(2);
 	}
 
-	if(listen(sock, 5) < 0)	//允许连接的最大数量为5
-	{
-		perror("listen");
-		exit(3);
-	}
+	// if(listen(sock, 5) < 0)	//允许连接的最大数量为5
+	// {
+	// 	perror("listen");
+	// 	exit(3);
+	// }
 
 	return sock;
 }
 
-int main(int argc,const char* argv[])
+int ppp(int port1)
 {
-	if(argc != 3)
-	{
-		printf("Usage:%s [loacl_ip] [loacl_port]\n",argv[0]);
-		return 1;
-	}
-
-	int listen_sock = startup(atoi(argv[2]),argv[1]);//初始化
-
-	//用来接收客户端的socket地址结构体
-	struct sockaddr_in remote;
-	socklen_t len = sizeof(struct sockaddr_in);
-
 	while(1)
 	{
-		int sock = accept(listen_sock, (struct sockaddr*)&remote, &len);
-		if(sock < 0)
+		char buf[1024];
+		struct sockaddr_in raddr;
+		int val = sizeof(struct sockaddr);
+		// ssize_t _s = read(sock, buf, sizeof(buf)-1);
+		ssize_t _s=recvfrom(sock, buf, sizeof(buf)-1, 0,(struct sockaddr*)&raddr,&val);
+		if(_s > 0)
 		{
-			perror("accept");
-			continue;
-		}
+			struct tcp_ss *tmp;
+			int len_ss = 0;
+		    FILE *file;
+		    char line[_LINE_LENGTH];
+		    char *ret = NULL;
+		    int ip_src_found = 0;
+		    int ip_dst_found = 0;
 
-		//每次建立一个连接后fork出一个子进程进行收发数据
-		pid_t id = fork();
-		if(id > 0)
-		{//father
-			close(sock);
-		//	while(waitpid(-1, NULL, WNOHANG) > 0);
-		}
-		else if(id == 0)
-		{//child
-			//printf("get a client, ip:%s, port:%d\n",inet_ntoa(remote.sin_addr),ntohs(remote.sin_port));
-			if(fork() > 0)
-				exit(0);
-			close(listen_sock);
-			char buf[1024];
-			while(1)
-			{
-				ssize_t _s = read(sock, buf, sizeof(buf)-1);
-				if(_s > 0)
-				{
-					struct tcp_ss *tmp;
-					int len_ss = 0;
-				    FILE *file;
-				    char line[_LINE_LENGTH];
-				    char *ret = NULL;
-				    int ip_src_found = 0;
-				    int ip_dst_found = 0;
+			len_ss = sizeof(struct tcp_ss) * sizeof(char);
+			tmp = (struct tcp_ss *)malloc(len_ss);
+			memset(tmp, 0, len_ss);
+			memcpy(tmp, buf, len_ss);
+			printf("%d %d %s %s\n", tmp->src_port, tmp->dst_port, tmp->ip_src, tmp->ip_dst);
 
-					len_ss = sizeof(struct tcp_ss) * sizeof(char);
-					tmp = (struct tcp_ss *)malloc(len_ss);
-					memset(tmp, 0, len_ss);
-					memcpy(tmp, buf, len_ss);
-					printf("%d %d %s %s\n", tmp->src_port, tmp->dst_port, tmp->ip_src, tmp->ip_dst);
+		    file = popen("ss -4 -t -i", "r");
+		    if (NULL != file)
+		    {
+		    	int found_cwnd = 0;
+		    	int cwnd = 0;
+		    	int mss = 0;
+		        while (fgets(line, _LINE_LENGTH, file) != NULL)
+		        {
+		            int index1 = 0;
+		            int index2 = 0;
+		            // printf("%s\n", line);
+		            index1 = strlinger(line, tmp->ip_src);
+		            index2 = strlinger(line, tmp->ip_dst);
+		           	if ((index1 > 0) && (index2 > 0))
+		            {
+		            	char *ret = NULL;
+		            	int index3 = 0;
+		            	int index4 = 0;
+		            	char * str = NULL;
+		            	int length_tmp = 0;
+		            	int port_src_found = 0;
+		            	int port_dst_found = 0;
+		            	
+		            	// printf("%s\n", line);
+		            	index3 = strlinger(line + index1, ":");
+		            	index3 = index3 + 1 + index1;
+		            	index4 = strlinger(line + index3, " ") + index3;
+		            	length_tmp = index4 - index3;
+		            	length_tmp = length_tmp * sizeof(char);
+		            	if (length_tmp > 0)
+		            	{
+		            		str = (char *)malloc(length_tmp);
+		            		memcpy(str, line + index3, length_tmp);
+		            		port_src_found = atoi(str);
+		            		free(str);
+		            		str = NULL;
+		            	}
+		            	else
+		            		continue;
+		            	// printf("%d %d\n", port_src_found, tmp->ip_src);
+		            	if(port_src_found != tmp->src_port)
+		            		continue;
+		            	index3 = strlinger(line + index4, ":") + index4;
+		            	index3 += 1;
+		            	index4 = strlinger(line + index3, " ") + index3;
+		            	length_tmp = index4 - index3;
+		            	length_tmp = length_tmp * sizeof(char);
+		            	// printf("%d %d %d\n", index3, index4, length_tmp);
+		            	if (length_tmp > 0)
+		            	{
+		            		str = (char *)malloc(length_tmp);
+		            		memcpy(str, line + index3, length_tmp);
+		            		// printf("111 %s\n", str);
+		            		if (strcmp("http", str) == 0)
+		            			port_dst_found = 80;
+		            		else if(strcmp("https", str) == 0)
+		            			port_dst_found = 8089;
+		            		else
+		            			port_dst_found = atoi(str);
+		            		free(str);
+		            		str = NULL;
+		            	}
+		            	else
+		            		continue;
+		            	// printf("%d\n", port_dst_found);
+		            	if(port_dst_found != tmp->dst_port)
+		            		continue;
+		            	ret = fgets(line, _LINE_LENGTH, file);
+		            	// printf("%s\n", line);
+		            	if(ret == NULL)
+		            		break;
+		            	else
+		            	{
+		            		index3 = strlinger(line, "cwnd");
+		            		index3 += 5;
+		            		index4 = strlinger(line + index3, " ") + index3;
+			            	length_tmp = index4 - index3;
+			            	length_tmp = length_tmp * sizeof(char);
+			            	// printf("%d %d %d\n", index3, index4, length_tmp);
+			            	if (length_tmp > 0)
+			            	{
+			            		str = (char *)malloc(length_tmp);
+			            		memcpy(str, line + index3, length_tmp);
+			            		cwnd = atoi(str);
+			            		free(str);
+			            		str = NULL;
+			            		found_cwnd = 1;
+			            	}
+			            	else
+			            		break;
+		            		index3 = strlinger(line, "mss");
+		            		index3 += 4;
+		            		index4 = strlinger(line + index3, " ") + index3;
+			            	length_tmp = index4 - index3;
+			            	length_tmp = length_tmp * sizeof(char);
+			            	// printf("%d %d %d\n", index3, index4, length_tmp);
+			            	if (length_tmp > 0)
+			            	{
+			            		str = (char *)malloc(length_tmp);
+			            		memcpy(str, line + index3, length_tmp);
+			            		mss = atoi(str);
+			            		free(str);
+			            		str = NULL;
+			            	}
+			            	else
+			            		break;
+		            	}
+		            }
+		            else
+		            {
+		            	continue;
+		            }
 
-				    file = popen("ss -4 -t -i", "r");
-				    if (NULL != file)
-				    {
-				    	int found_cwnd = 0;
-				    	int cwnd = 0;
-				    	int mss = 0;
-				        while (fgets(line, _LINE_LENGTH, file) != NULL)
-				        {
-				            int index1 = 0;
-				            int index2 = 0;
+		        }
+		        if(found_cwnd == 1)
+		        {
+		        	char buf[1024];
+		        	struct cwnd_mss tmp;
+		        	tmp.cwnd = cwnd;
+		        	tmp.mss = mss;
+		        	int leng_int = sizeof(struct cwnd_mss);
+		        	memcpy(buf, &tmp, leng_int);
+		        	write(sock, buf, leng_int);
+		        }
+		        else
+		        {
+		        	char buf[1024];
+		        	struct cwnd_mss tmp;
+		        	tmp.cwnd = -1;
+		        	tmp.mss = -1;
+		        	int leng_int = sizeof(struct cwnd_mss);
+		        	memcpy(buf, &tmp, leng_int);
+		        	write(sock, buf, leng_int);				        	
+		        }
+		    }
+		    else
+		        continue;
+		    free(ret);
+		    ret = NULL; 
+		    pclose(file);
 
-				            index1 = strlinger(line, tmp->ip_src);
-				            index2 = strlinger(line, tmp->ip_dst);
-				           	if ((index1 > 0) && (index2 > 0))
-				            {
-				            	char *ret = NULL;
-				            	int index3 = 0;
-				            	int index4 = 0;
-				            	char * str = NULL;
-				            	int length_tmp = 0;
-				            	int port_src_found = 0;
-				            	int port_dst_found = 0;
-				            	
-				            	// printf("%s\n", line);
-				            	index3 = strlinger(line + index1, ":");
-				            	index3 = index3 + 1 + index1;
-				            	index4 = strlinger(line + index3, " ") + index3;
-				            	length_tmp = index4 - index3;
-				            	length_tmp = length_tmp * sizeof(char);
-				            	if (length_tmp > 0)
-				            	{
-				            		str = (char *)malloc(length_tmp);
-				            		memcpy(str, line + index3, length_tmp);
-				            		port_src_found = atoi(str);
-				            		free(str);
-				            		str = NULL;
-				            	}
-				            	else
-				            		continue;
-				            	// printf("%d %d\n", port_src_found, tmp->ip_src);
-				            	if(port_src_found != tmp->src_port)
-				            		continue;
-				            	index3 = strlinger(line + index4, ":") + index4;
-				            	index3 += 1;
-				            	index4 = strlinger(line + index3, " ") + index3;
-				            	length_tmp = index4 - index3;
-				            	length_tmp = length_tmp * sizeof(char);
-				            	// printf("%d %d %d\n", index3, index4, length_tmp);
-				            	if (length_tmp > 0)
-				            	{
-				            		str = (char *)malloc(length_tmp);
-				            		memcpy(str, line + index3, length_tmp);
-				            		// printf("111 %s\n", str);
-				            		if (strcmp("http", str) == 0)
-				            			port_dst_found = 80;
-				            		else if(strcmp("https", str) == 0)
-				            			port_dst_found = 8089;
-				            		else
-				            			port_dst_found = atoi(str);
-				            		free(str);
-				            		str = NULL;
-				            	}
-				            	else
-				            		continue;
-				            	// printf("%d\n", port_dst_found);
-				            	if(port_dst_found != tmp->dst_port)
-				            		continue;
-				            	ret = fgets(line, _LINE_LENGTH, file);
-				            	// printf("%s\n", line);
-				            	if(ret == NULL)
-				            		break;
-				            	else
-				            	{
-				            		index3 = strlinger(line, "cwnd");
-				            		index3 += 5;
-				            		index4 = strlinger(line + index3, " ") + index3;
-					            	length_tmp = index4 - index3;
-					            	length_tmp = length_tmp * sizeof(char);
-					            	// printf("%d %d %d\n", index3, index4, length_tmp);
-					            	if (length_tmp > 0)
-					            	{
-					            		str = (char *)malloc(length_tmp);
-					            		memcpy(str, line + index3, length_tmp);
-					            		cwnd = atoi(str);
-					            		free(str);
-					            		str = NULL;
-					            		found_cwnd = 1;
-					            	}
-					            	else
-					            		break;
-				            		index3 = strlinger(line, "mss");
-				            		index3 += 4;
-				            		index4 = strlinger(line + index3, " ") + index3;
-					            	length_tmp = index4 - index3;
-					            	length_tmp = length_tmp * sizeof(char);
-					            	// printf("%d %d %d\n", index3, index4, length_tmp);
-					            	if (length_tmp > 0)
-					            	{
-					            		str = (char *)malloc(length_tmp);
-					            		memcpy(str, line + index3, length_tmp);
-					            		mss = atoi(str);
-					            		free(str);
-					            		str = NULL;
-					            	}
-					            	else
-					            		break;
-				            	}
-				            }
-				            else
-				            {
-				            	continue;
-				            }
-
-				        }
-				        if(found_cwnd == 1)
-				        {
-				        	char buf[1024];
-				        	struct cwnd_mss tmp;
-				        	tmp.cwnd = cwnd;
-				        	tmp.mss = mss;
-				        	int leng_int = sizeof(struct cwnd_mss);
-				        	memcpy(buf, &tmp, leng_int);
-				        	write(sock, buf, leng_int);
-				        }
-				        else
-				        {
-				        	char buf[1024];
-				        	struct cwnd_mss tmp;
-				        	tmp.cwnd = -1;
-				        	tmp.mss = -1;
-				        	int leng_int = sizeof(struct cwnd_mss);
-				        	memcpy(buf, &tmp, leng_int);
-				        	write(sock, buf, leng_int);				        	
-				        }
-				    }
-				    else
-				        continue;
-				    free(ret);
-				    ret = NULL; 
-				    pclose(file);
-
-					free(tmp);
-					tmp = NULL;
-				}
-				else
-				{
-					printf("client is quit!\n");
-					break;
-				}
-			}
+			free(tmp);
+			tmp = NULL;
 		}
 		else
 		{
-			perror("fork");
-			return 2;
+			sleep(1);
+			continue;
 		}
 	}
 	return 0;
 }
+
+int main()
+{
+	int port;
+	int ret = 0;
+	port = 12345;
+	while(1)
+	{
+		ret = ppp(port);
+	}
+	
+	return 0;
+}	
