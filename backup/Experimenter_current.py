1947572ca9017c7b7a011c6b7eb1104eed4cfa00
lin <lin@lin>
2018-11-01 00:13:51 -0700

    old codes for the first paper

---
+++ backup/Experimenter_current.py
@@ -0,0 +1,221 @@
+# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+# import newanalysis
+# import get_tree
+import time as time_linger
+# import struct
+# from ryu import utils
+# from ryu.ofproto import ether
+# from ryu.ofproto import inet
+from ryu.ofproto.ofproto_v1_2 import OFPG_ANY
+# from ryu.lib import hub
+
+# import pandas as pd
+# from pandas import Series
+# import numpy as np
+# import pydotplus
+# from sklearn import tree
+# from sklearn.externals.six import StringIO
+# from sklearn.metrics import precision_recall_curve
+# from sklearn.metrics import classification_report
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import precision_recall_fscore_support as score
+# import re
+# import ctypes
+# import subprocess
+# import threading
+
+
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        self.abc = -1
+        # self.thread_list = []
+        # for i in xrange(5):
+        #     print i, "here"
+        #     sthread = threading.Thread(
+        #         target=self._get_realtime_data)
+        #     sthread.start()
+        #     self.thread_list.append(sthread)
+        # print "after spawn"
+
+    # def _get_realtime_data(self):
+    #     ll = ctypes.cdll.LoadLibrary
+    #     lib = ll("/home/lin/controller_project/receivea.so")
+    #     # lib.fack(number)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.  The bug has been fixed in OVS v2.1.0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+        # print "switch_features_handler"
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        # print "_packet_in_handler"
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
+        tmp = (int(time_linger.time()) % 3)
+        if(tmp == 0):
+        	if (self.abc == 0):
+	            try:
+	                ftmp = open('tree.csv', 'rb')
+	                tree = ftmp.readline()
+	                # print tree
+	            except Exception:
+	                print 'tree.csv open failed'
+	            if tree != "":
+	            	self.abc = -1
+	                print "------------------------------------------------------------------"
+	                print tree
+	                self.install_experimental_flow(tree, ev)
+        else:
+        	self.abc = 0
+
+    def install_experimental_flow(self, tree, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        parser = datapath.ofproto_parser
+
+        dpiActionRTSP = parser.OFPExperimenter(
+            datapath, None, None, tree)
+
+        datapath.send_msg(dpiActionRTSP)
+        # print dpiActionRTSP
+
+    def ipv4_to_str(self, integre):
+        ip_list = [str((integre >> (24 - (n * 8)) & 255)) for n in range(4)]
+        return '.'.join(ip_list)
+
+    def ipv4_to_int(self, string):
+        ip = string.split('.')
+        assert len(ip) == 4
+        i = 0
+        for b in ip:
+            b = int(b)
+            i = (i << 8) | b
+        return i
+
+    def create_match(self, parser, fields):
+        """Create OFP match struct from the list of fields."""
+        match = parser.OFPMatch()
+        for a in fields:
+            match.append_field(*a)
+        return match
+
+    def create_flow_mod(self, datapath, idle_timeout, hard_timeout, priority,
+                        table_id, match, instructions):
+        """Create OFP flow mod message."""
+        ofproto = datapath.ofproto
+        # print datapath.id
+        flow_mod = datapath.ofproto_parser.OFPFlowMod(datapath, 0, 0,
+                                                      table_id,
+                                                      ofproto.OFPFC_ADD,
+                                                      idle_timeout,
+                                                      hard_timeout, priority,
+                                                      ofproto.OFPCML_NO_BUFFER,
+                                                      ofproto.OFPP_ANY,
+                                                      OFPG_ANY, 0,
+                                                      match, instructions)
+        return flow_mod
---
+++ backup/README.txt
@@ -0,0 +1,6 @@
+name 						location		useage
+Experimenter_current.py  	/ryu/ryu/app/ 	ryu-manager Experimenter_current.py
+flow.c  					/openvswitch-2.8.1/datapath/flow.c
+ofp-msgs.h  				/openvswitch-2.8.1/include/openvswitch/
+ofproto.c  					/openvswitch-2.8.1/ofproto/
+tree.csv					/ryu/ryu/app
